23274284e4fe86838678dde823f0a9c09ea32a74
tohidshabanloo <tshabanloo@hotmail.com>
2022-05-06 21:49:12 +0430

    footer added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from "./logo.svg";
 import "./App.css";
 import LoginForm from "./component/LoginForm/LoginForm";
+import Footer from "./component/Footer/Footer";
 
 function App() {
   return (
     <div className="App">
       <LoginForm />
+      <Footer />
     </div>
   );
 }
